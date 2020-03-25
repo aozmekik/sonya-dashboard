dbf24793fcca86e2e63e0447c285c30db018e855
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-03-25 13:09:00 +0300

    small refactoring

--- app_public/src/app/pages/accounting/reports/reports.component.ts
+++ app_public/src/app/pages/accounting/reports/reports.component.ts
@@ -7,18 +7,24 @@ import {ActivitiesData} from '../activities-data';
   styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-  public activityTypes = Object.keys(ActivitiesData.ActivityTypes)
-    .map(key => ActivitiesData.ActivityTypes[key]);
-  public activityAbouts = Object.keys(ActivitiesData.ActivityAbouts)
-    .map(key => ActivitiesData.ActivityAbouts[key]);
-  public activityBankSafes = Object.keys(ActivitiesData.ActivityBankSafes)
-    .map(key => ActivitiesData.ActivityBankSafes[key]);
-  public currentActivityType = 'Gelir';
-  public currentBankSafe = 'Banka';
-  public currentAbouts = 'Üye';
+  // public activityTypes = Object.keys(ActivitiesData.ActivityTypes)
+  //   .map(key => ActivitiesData.ActivityTypes[key]);
+  // public activityAbouts = Object.keys(ActivitiesData.ActivityAbouts)
+  //   .map(key => ActivitiesData.ActivityAbouts[key]);
+  // public activityBankSafes = Object.keys(ActivitiesData.ActivityBankSafes)
+  //   .map(key => ActivitiesData.ActivityBankSafes[key]);
+  public activityTypes = this.table2array(ActivitiesData.ActivityTypes);
+  public activityAbouts = this.table2array(ActivitiesData.ActivityAbouts);
+  public activityBankSafes = this.table2array(ActivitiesData.ActivityBankSafes);
+  public currentActivityType = this.activityTypes[0];
+  public currentBankSafe = this.activityBankSafes[0];
+  public currentAbouts = this.activityAbouts[0];
   public currentCampaign = 'campaign';
-  constructor() {}
 
+  constructor() {}
+  private table2array(table): string[] {
+    return Object.keys(table).map(key => table[key]);
+  }
   ngOnInit() {
   }
 
