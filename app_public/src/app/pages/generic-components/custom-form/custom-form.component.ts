415824bd54c9eedcdb76c471f6f711c232a55b02
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-08-02 12:48:11 +0300

    fixed some problems preventing build

--- app_public/src/app/pages/generic-components/custom-form/custom-form.component.ts
+++ app_public/src/app/pages/generic-components/custom-form/custom-form.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
-@Component({
-  selector: 'ngx-custom-form',
-  template: '',
-})
+
 export abstract class CustomFormComponent<T> implements OnInit {
 
   @Input() model: T;
--- app_public/src/app/pages/generic-components/generic-components.module.ts
+++ app_public/src/app/pages/generic-components/generic-components.module.ts
@@ -1,7 +1,7 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomSelectorComponent } from './custom-form/form-rows/custom-selector/custom-selector.component';
 import { CustomDatePickerComponent } from './custom-form/form-rows/custom-date-picker/custom-date-picker.component';
-import { NgModule, Input } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbSelectModule, NbDatepickerModule, NbInputModule, } from '@nebular/theme';
 import { CustomInputBoxComponent } from './custom-form/form-rows/custom-input-box/custom-input-box.component';
