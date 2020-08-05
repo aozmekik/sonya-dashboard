a36691ae4dff7cc7bc3a9a135ee74d2c02c5bb6d
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-08-05 17:59:32 +0300

    added a toast information when submitting form

--- app_public/src/app/pages/generic-components/custom-form/custom-form.component.ts
+++ app_public/src/app/pages/generic-components/custom-form/custom-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { NbComponentStatus, NbGlobalPhysicalPosition, NbToastrService, NbGlobalPositionStrategy } from '@nebular/theme';
 
 
 export abstract class CustomFormComponent<T> implements OnInit {
@@ -9,7 +10,24 @@ export abstract class CustomFormComponent<T> implements OnInit {
   @Input() disable: string[];
   public form: FormGroup;
 
-  constructor(public formBuilder: FormBuilder) { }
+  public readonly types: NbComponentStatus[] = [
+    'primary',
+    'success',
+    'info',
+    'warning',
+    'danger',
+  ];
+
+  public config = {
+    status: null, /* will be assigned run-time */
+    destroyByClick: true, /* toast can be destroyed by click */
+    duration: 2000, /* 2 seconds */
+    hasIcon: true, /* an insignificant detail */
+    position: NbGlobalPhysicalPosition.TOP_RIGHT, /* toast will be on top-right */
+    preventDuplicates: false, /* duplicate is possible */
+  };
+
+  constructor(public formBuilder: FormBuilder, public toastrService: NbToastrService) { }
 
   ngOnInit() {
     /* those keys are strict literals and hard-coded in the .html file */
@@ -19,7 +37,16 @@ export abstract class CustomFormComponent<T> implements OnInit {
         this.form.controls[d].disable();
   }
 
-  abstract onSubmit(): void;
+  public showToast(type: NbComponentStatus, title: string, body: string) {
+    this.config.status = type;
+    this.toastrService.show(body, title, this.config);
+  }
+
+  public onSubmit(): void {
+    console.log(this.form.value); // FIXME. carefull with disabled fields.
+    this.showToast(this.types[1], 'İşlem Başarılı!',
+      `${this.buttonName}me İşlemi Başarı İle Gerçekleştirildi!`);
+  }
 
 
 }
