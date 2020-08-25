c1d54e8f5fa83fab0e728fc95bd6c17b66544ae2
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-08-25 19:45:55 +0300

    Revert "Revert "Revert "Revert "update 1, serious commit""""
    This reverts commit b400e7bb472ea89ebefff63fb670bfadd7d7c05b.

--- app_public/src/app/pages/families/family-form/family-form.component.ts
+++ app_public/src/app/pages/families/family-form/family-form.component.ts
@@ -25,6 +25,8 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
   public statuses: string[] = Utils.keys(Family.statutes);
 
   private destroy$ = new Subject();
+  public income: number = 0;
+  public outgo: number = 0;
 
   constructor(public formBuilder: FormBuilder, public toastrService: NbToastrService) {
     super(formBuilder, toastrService);
@@ -42,7 +44,9 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
       'status': new FormControl(this.model.status),
       'comment': new FormControl(this.model.comment),
       'members': new FormArray([]),
-      'income': new FormControl(this.model.income),
+      'incomeCount': new FormControl(this.income),
+      'outgoCount': new FormControl(this.outgo),
+      'income': new FormControl([]),
     });
 
     this.memberCount.valueChanges
@@ -53,6 +57,14 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
         this.generateMemberForm();
       });
 
+    this.incomeCount.valueChanges
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => {
+        this.generateIncomeForm();
+      });
+
 
     this.memberCount.valueChanges
       .pipe(
@@ -61,14 +73,38 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
       .subscribe(() => {
         if (this.memberCount.value >= this.model.members.length) {
           this.model.members.forEach(member => {
-            this.members.push(this.formBuilder.group({ name: member.name, age: member.age }))
+            this.members.push(this.formBuilder.group(
+              {
+                idNo: member.idNo,
+                name: member.name,
+                birthyear: member.birthyear,
+                gender: member.gender,
+                job: member.job,
+                income: member.income,
+                body: member.body,
+                shoe: member.shoe,
+                disease: member.disease,
+              }
+            ))
           });
         }
       });
 
     if (this.model.members) {
       this.model.members.forEach(member => {
-        this.members.push(this.formBuilder.group({ name: member.name, age: member.age }))
+        this.members.push(this.formBuilder.group(
+          {
+            idNo: member.idNo,
+            name: member.name,
+            birthyear: member.birthyear,
+            gender: member.gender,
+            job: member.job,
+            income: member.income,
+            body: member.body,
+            shoe: member.shoe,
+            disease: member.disease,
+          }
+        ))
       });
     }
 
@@ -89,7 +125,7 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
     else
       count = this.memberCount.value;
 
-    if (this.memberCount.value <= 99) {
+    if (this.memberCount.value <= 10) {
       for (let x = 0; x < count; x++) {
         this.members.push(this.createMember());
       }
@@ -98,10 +134,44 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
 
   createMember() {
     return new FormGroup({
-      'name': new FormControl(null, { validators: [Validators.required,] }),
-      'age': new FormControl(null, { validators: [Validators.required, Validators.maxLength(3)] })
+      'idNo': new FormControl(null, { validators: [Validators.required] }),
+      'name': new FormControl(null, { validators: [Validators.required] }),
+      'birthyear': new FormControl(null, { validators: [Validators.required, Validators.minLength(4), Validators.maxLength(4)] }),
+      'gender': new FormControl(null, { validators: [Validators.required] }),
+      'job': new FormControl(null, { validators: [Validators.required] }),
+      'income': new FormControl(null, { validators: [Validators.required] }),
+      'body': new FormControl(null, { validators: [Validators.required] }),
+      'shoe': new FormControl(null, { validators: [Validators.required] }),
+      'disease': new FormControl(null, { validators: [Validators.required] }),
     });
   }
+
+  generateIncomeForm() {
+    this.incomes.clear();
+    let count;
+    if (this.model.incomes) {
+      count = this.incomeCount.value >= this.model.incomes.length ?
+        this.incomeCount.value - this.model.members.length :
+        this.incomeCount.value;
+    }
+    else
+      count = this.incomeCount.value;
+
+    if (this.incomeCount.value <= 10) {
+      for (let x = 0; x < count; x++) {
+        this.incomes.push(this.createIncome());
+      }
+    }
+  }
+
+
+  createIncome() {
+    return new FormGroup({
+      'name': new FormControl(null, { validators: [Validators.required] }),
+      'amount': new FormControl(null, { validators: [Validators.required] }),
+    });
+  }
+
 
   get memberCount() {
     return this.form.get('memberCount');
@@ -109,5 +179,13 @@ export class FamilyFormComponent extends CustomFormComponent<Family> implements 
 
   get members() {
     return this.form.get('members') as FormArray;
+  }
+
+  get incomeCount() {
+    return this.form.get('incomeCount');
+  }
+
+  get incomes() {
+    return this.form.get('incomes') as FormArray;
   }
 }
