07b151c738ceb8791de699c0b2f5fa4e7a11a8ad
Ahmed Semih Özmekik <ahmed-semih@outlook.com>
2020-08-25 18:18:36 +0300

    started some family adjustments

--- app_public/src/app/pages/families/families-data.ts
+++ app_public/src/app/pages/families/families-data.ts
@@ -1,4 +1,5 @@
 import { Family } from "./family";
+import { Member } from '../members/member';
 
 // only for representative frontend. will be deleted soon.
 export class FamiliesData {
@@ -9,12 +10,38 @@ export class FamiliesData {
       regDate: new Date().toLocaleString(),
       memberCount: 2,
       area: Family.Area.ANATOLIA,
+      nation: Member.Nation.Syrian,
       address: "address1",
       registeredMember: "member1",
       status: Family.Status.UNAIDED,
-      members: [{name: "membera", age:12}, {name: "memberb", age:6}],
+      members: [{
+        idNo: 1239,
+        name: "namea",
+        birthyear: 1999,
+        gender: Member.Gender.MALE,
+        job: "job1",
+        income: 123,
+        body: Family.Body.S,
+        shoe: 41,
+        disease: "engel1",
+      },
+      {
+        idNo: 12329,
+        name: "nameb",
+        birthyear: 1999,
+        gender: Member.Gender.FEMALE,
+        job: "job2",
+        income: 123,
+        body: Family.Body.M,
+        shoe: 41,
+        disease: "engel2",
+      }
+      ],
       comment: "comment1",
-      income: 500,
+      incomes: [{ name: "income1", amount: 100 }],
+      educations: [{ name: "isim", school: "okul", grade: 5 }],
+      outgoes: [{ name: "outgo1", amount: 200 }],
+      bills: [{name: "bill1", contract: 100}]
     },
     {
       _id: "id",
@@ -24,10 +51,36 @@ export class FamiliesData {
       area: Family.Area.EUROPE,
       address: "address1",
       registeredMember: "member2",
+      nation: Member.Nation.Syrian,
       status: Family.Status.UNIDENTIFIED,
-      members: [{name: "membera", age:40}, {name: "memberb", age:12}, {name: "memberc", age:12}],
       comment: "comment2",
-      income: 100,
+      members: [{
+        idNo: 1239,
+        name: "namea",
+        birthyear: 1999,
+        gender: Member.Gender.MALE,
+        job: "job1",
+        income: 123,
+        body: Family.Body.S,
+        shoe: 41,
+        disease: "engel1",
+      },
+      {
+        idNo: 12329,
+        name: "nameb",
+        birthyear: 1999,
+        gender: Member.Gender.FEMALE,
+        job: "job2",
+        income: 123,
+        body: Family.Body.M,
+        shoe: 41,
+        disease: "engel2",
+      },
+      ],
+      incomes: [{ name: "income1", amount: 100 }],
+      educations: [{ name: "isim", school: "okul", grade: 5 }],
+      outgoes: [{ name: "outgo1", amount: 200 }],
+      bills: [{name: "bill1", contract: 100}]
     },
   ];
 
